47fd990736677af340911ba8e9e3fcbd6612845d
Simon Podlipsky <simon@podlipsky.net>
2020-10-25 12:23:02 +0100

    Assert the error is thrown

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -7,14 +7,24 @@ use PHPUnit\Framework\TestCase;
 
 final class PluginTest extends TestCase
 {
-    public function test_no_error() {
+    public function test_no_error(): void {
         chdir(__DIR__.'/../');
         $returnCode = -1;
         $output = [];
         exec('vendor/bin/psalm -c psalm-test.xml --no-progress --no-cache --output-format=json tests/testcases/NoError.php', $output, $returnCode);
         $outputString = self::indentJson(implode("\n", $output));
         self::assertEmpty($outputString, $outputString);
-        self::assertEquals(0, $returnCode);
+        self::assertSame(0, $returnCode);
+    }
+
+    public function test_error(): void {
+        chdir(__DIR__.'/../');
+        $returnCode = -1;
+        $output = [];
+        exec('vendor/bin/psalm -c psalm-test.xml --no-progress --no-cache --output-format=json tests/testcases/Error.php', $output, $returnCode);
+        $outputString = self::indentJson(implode("\n", $output));
+        self::assertNotEmpty($outputString, $outputString);
+        self::assertSame(1, $returnCode);
     }
 
     public static function indentJson(string $json): string {
